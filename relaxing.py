e57cf1094fbefa2b40ccd9425893e47ca9238b8f
Martina <114102344+martirrrrr@users.noreply.github.com>
2024-12-17 11:41:45 +0100

    Update relaxing.py

--- relaxing.py
+++ relaxing.py
@@ -34,19 +34,30 @@ def speech_recognition(ip, port):
         except RuntimeError:
             print("[INFO] Il modulo 'Test_ASR' non era sottoscritto. Procedo...")
 
+                # Imposta il vocabolario
+        speech_recognition.setVocabulary(vocabulary, False)  # False per evitare il riconoscimento casuale
+        print("[INFO] Vocabolario impostato con successo.")
+
+        # Riattiva il motore ASR
+        speech_recognition.subscribe("Test_ASR")
+        speech_recognition.pause(False)
+        print("[INFO] Riconoscimento vocale attivato. Pronuncia una parola...")
+
+
                 # Contatore delle interazioni
         interaction_count = 0
         max_interactions = 30  # Numero massimo di richieste
 
         while interaction_count < max_interactions:
             # Aspetta che Pepper riconosca una parola
+            text_to_speech.say("Vogliamo incominciare?")
             word_recognized = memory.getData("WordRecognized")
             if word_recognized:
                 recognized_word = word_recognized[0]  # La parola riconosciuta
                 confidence = word_recognized[1]      # Confidenza del riconoscimento
                 if confidence > 0.5:  # Filtro per evitare falsi positivi
-                    print(f"[INFO] Hai detto: {recognized_word}")
-                    text_to_speech.say(f"Hai detto {recognized_word}")
+                    print("[INFO] Hai detto: {recognized_word}")
+                    text_to_speech.say("Hai detto {recognized_word}")
                     interaction_count += 1  # Incrementa il contatore
                     
                     if recognized_word == "sì":
@@ -118,7 +129,7 @@ def speech_recognition2(ip, port):
                 memory.insertData("WordRecognized", None)
 
     except KeyboardInterrupt:
-        print("\n[INFO] Programma interrotto manualmente.")
+        print("[INFO] Programma interrotto manualmente.")
         speech_recognition.unsubscribe("Meditation_ASR")
         text_to_speech.say("Alla prossima!")
 
@@ -191,4 +202,6 @@ if __name__ == "__main__":
     ROBOT_IP = "192.168.1.104"  # Sostituisci con l'indirizzo IP di Pepper
     ROBOT_PORT = 9559           # Porta predefinita di Pepper
 
+    text_to_speech.say("Benvenuto")
+    #text_to_speech.say("Benvenuto in questa sessione di meditazione.")
     speech_recognition(ROBOT_IP, ROBOT_PORT)
