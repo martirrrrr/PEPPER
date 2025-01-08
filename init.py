045cdb294f1b44c981382ea60b3077f724ec1849
Martina <114102344+martirrrrr@users.noreply.github.com>
2025-01-08 11:58:35 +0100

    Create init.py

---
+++ init.py
@@ -0,0 +1,46 @@
+# script_principale.py
+
+from happy import main as happy_main
+from sad import main as sad_main
+from relax import main as relax_main
+from neutral import main as neutral_main
+
+def read_value(path):
+    try:
+        with open(path, 'r') as file:
+            val = file.read().strip()
+            return int(val)
+    except FileNotFoundError:
+        print(f"Errore: Il file '{path}' non è stato trovato.")
+    except ValueError:
+        print(f"Errore: Il contenuto del file '{path}' non è un numero valido.")
+    return None
+
+def main():
+    path = 'stat.txt'  # Sostituisci con il percorso corretto del tuo file
+    val = read_val(path)
+
+    if val is None:
+        return
+
+    if val == 0:
+        happy_main()
+    elif val == 1:
+        sad_main()
+    elif val == 2:
+        relax_main()
+    elif val == 3:
+        neutral = neutral_main()
+        if neutral == 0:
+            happy_main()
+        elif neutral == 1:
+            sad_main()
+        elif neutral == 2:
+            relax_main()
+        else:
+            print("Valore restituito da neutral.py non valido.")
+    else:
+        print("Valore nel file non riconosciuto.")
+
+if __name__ == "__main__":
+    main()
