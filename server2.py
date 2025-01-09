f60e94e542290bee217b7eceeebb85d9058f40e5
Martina <114102344+martirrrrr@users.noreply.github.com>
2025-01-09 12:06:07 +0100

    Update server2.py

--- server2.py
+++ server2.py
@@ -1,33 +1,34 @@
 import socket
 
-# Configurazione del server
-HOST = '0.0.0.0'  # Ascolta su tutte le interfacce di rete
-PORT = 12345      # Porta su cui ascoltare
+# Server configuration
+HOST = '0.0.0.0'  # Listen on all available IP addresses
+PORT = 5000       # Port to listen on
 
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(1)  # Ascolta una connessione alla volta
-    print("Server in ascolto su {}:{}".format(HOST, PORT))
-    
-    try:
-        while True:
-            conn, addr = server_socket.accept()
-            print("Connesso da:", addr)
-            
-            # Riceve e stampa i dati
-            data = conn.recv(1024)
-            if not data:
-                break
-            print("Dati ricevuti:", data)
-            
-            # Invia una risposta al client
-            conn.sendall(b"Ricevuto: " + data)
-            conn.close()
-    except KeyboardInterrupt:
-        print("Chiusura del server...")
-    finally:
-        server_socket.close()
+# Create the socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(1)
 
-if _name_ == "_main_":
-    start_server()
+print("Server is listening on port {}".format(PORT))
+
+try:
+    while True:
+        # Accept connections from the client
+        conn, addr = server_socket.accept()
+        print("Connection accepted from: {}".format(addr))
+        
+        # Receive data from the client
+        data = conn.recv(1024)
+        if not data:
+            break
+        print("Data received: {}".format(data))
+        
+        # Send a response back to the client
+        response = "Data received successfully"
+        conn.send(response)
+
+        conn.close()
+except KeyboardInterrupt:
+    print("Server terminated manually.")
+finally:
+    server_socket.close()
